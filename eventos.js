9224a250b42f2cf6daa1a8ef5ee47905d90b50ea
gastontechera <gastontecheram@gmail.com>
2023-08-11 14:11:55 -0300

    Eventos.js

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const divBoton = document.querySelector("div");
+
+    divBoton.addEventListener('click', () => {
+        alert("Hola! Soy el div");
+    });
+});
