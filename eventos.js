e58fcf2d7f7fe69805da1c38a8cb635dec3d1e57
gastontechera <gastontecheram@gmail.com>
2023-08-11 14:49:37 -0300

    Event Propagation
    Se agrega "eventListener" para el botón y se agrega "eventPropagation" en el archivo javascript. Se elimina "onclick" en el archivo html

--- eventos.js
+++ eventos.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const divBoton = document.querySelector("div");
+    const button = document.querySelector("button");
 
     divBoton.addEventListener('click', () => {
         alert("Hola! Soy el div");
     });
+
+    button.addEventListener('click', (event) => {
+        alert("como estas");
+        event.stopPropagation();
+    });
+
 });
